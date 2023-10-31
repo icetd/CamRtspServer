fc62a626d9e2b15036389314a30d5768cdfde2a5
icetd <tiandengzbc@gmail.com>
2023-11-01 07:08:36 +0800

    add log set

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,25 @@
 #include <iostream>
 #include "CamRTSPServer.h"
+#include "INIReader.h"
 #include "log.h"
 
 int LogLevel;
 int main()
 {	
-	
-	initLogger(NOTICE);
+    INIReader configs("./configs/config.ini");
+    if (configs.ParseError() < 0) {
+		printf("read config failed.");
+		exit(1);
+	} else {
+        std::string level = configs.Get("log", "level", "NOTICE");
+		if (level == "NOTICE") {
+			initLogger(NOTICE);
+		} else if(level == "INFO") {
+			initLogger(INFO);
+		} else if(level == "ERROR") {
+			initLogger(ERROR);
+		}
+    }
 
 	try {
 		CamRTSPServer server;
